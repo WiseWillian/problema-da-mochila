65de975facf132954bd80675881e5a362e03c339
WiseWillian <rafael.willian.engcomp@gmail.com>
2018-04-10 20:56:34 -0300

    Comit inicial com o setup da solução

---
+++ cmake-build-debug/CMakeFiles/Problema_da_mochila.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Problema_da_mochila.dir/main.c.o"
+  "Problema_da_mochila.pdb"
+  "Problema_da_mochila"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Problema_da_mochila.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
